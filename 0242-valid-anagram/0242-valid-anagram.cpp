9257eebe7327d87d12f30e249852f6a030be4fe4
Rushang Pravin Chandekar <142429467+rushangchandekar@users.noreply.github.com>
2026-07-23 20:32:53 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector <int> count(26,0);
+        for(char c:s){
+            count[c-'a']++;
+        }
+        for(char c:t){
+            count[c-'a']--;
+        }
+        for (int x:count){
+            if(x!=0) return false;
+        }
+        return true;
+    }
+};
