74a2a92a34c7d4b0ce93266892d702ca845e298b
Rushang Pravin Chandekar <142429467+rushangchandekar@users.noreply.github.com>
2026-09-01 14:51:03 +0530

    Time: 71 ms (5.17%), Space: 17.5 MB (97.45%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath,
+                    vector<vector<int>>& edges) {
+
+        vis[src] = true;
+        recPath[src] = true;
+
+        for(int i = 0; i < edges.size(); i++) {
+
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if(u == src) {
+                if(!vis[v]) {
+                    if(isCycleDFS(v, vis, recPath, edges)) {
+                        return true;
+                    }
+                } else if(recPath[v]) { //BACK EDGE -> Cycle
+                    return true;
+                }
+            }
+        }
+
+        recPath[src] = false;
+        return false;
+    }
+
+    void topoOrder(int src, vector<bool> &vis, stack<int> &s, vector<vector<int>>& edges) {
+        vis[src] = true;
+
+        for(int i = 0; i < edges.size(); i++) {
+
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if(u == src) {
+                if(!vis[v]) {
+                    topoOrder(v, vis, s, edges);
+                }
+            }
+        }
+
+        s.push(src);
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+
+        vector<bool> vis(n, false);
+        vector<bool> recPath(n, false);
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                if(isCycleDFS(i, vis, recPath, edges)) {
+                    return ans; //False i.e. Empty array 
+                }
+            }
+        }
+
+        //Topological sorted order
+        stack<int> s;
+        vis.assign(n, false);
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                topoOrder(i, vis, s, edges);
+            }
+        }
+
+        while(s.size() > 0) {
+            ans.push_back(s.top());
+            s.pop();
+        }
+
+        return ans;
+    }
+};
