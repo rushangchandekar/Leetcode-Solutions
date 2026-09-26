ac7fe5336efebe420e3878a7e77d98e7653e9c89
Rushang Pravin Chandekar <142429467+rushangchandekar@users.noreply.github.com>
2026-09-26 18:11:41 +0530

    Time: 0 ms (100%), Space: 40.3 MB (42.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,6 +4,22 @@ public:
         int n = nums.size();
         vector<int> ans(n, 1);
 
+        // vector<int> prefix(n, 1);
+        // vector<int> suffix(n, 1);
+
+        // for (int i=1; i<n; i++) {
+        //     prefix[i] = prefix[i-1] * nums[i-1]; 
+        // }
+
+        // for (int i=n-2; i>=0; i--) {
+        //     suffix[i] = suffix[i+1] * nums[i+1];
+        // }
+
+        // for (int i=0; i<n; i++) {
+        //     ans[i] = prefix[i] * suffix[i];
+        // }
+
+        //Optimal Approach
         //prefix => ans
         for (int i=1; i<n; i++) {
             ans[i] = ans[i-1] * nums[i-1]; 
